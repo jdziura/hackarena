3460adcf2cc57f70d39916ef857152f79e73426f
Bombini <antekbom@gmail.com>
2024-09-22 12:37:02 +0200

    Updated info in agent.h

--- src/agent/agent.h
+++ src/agent/agent.h
@@ -4,11 +4,13 @@
 
 class Agent {
  public:
+    // Take overhead into account !!!
+
     /// DO NOT DELETE
-    /// lifetime of a thread with NextMove() in milliseconds
+    /// lifetime of a thread with NextMove() in milliseconds, CAN BE CHANGED WHENEVER YOU WANT
     int timeoutNumber = 200;
     /// DO NOT DELETE
-    /// time in milliseconds after which the NextMove() answer is not sent to server
+    /// time in milliseconds after which the NextMove() answer is not sent to server, CAN BE CHANGED WHENEVER YOU WANT
     int skipResponse = 100;
 
 	Agent();
