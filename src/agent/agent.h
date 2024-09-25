243ff8dc217c7ab985b7733c77d7ec773dd17282
Bombini <antekbom@gmail.com>
2024-09-25 22:07:43 +0200

    Fixed info in agent.h

--- src/agent/agent.h
+++ src/agent/agent.h
@@ -6,7 +6,7 @@ class Agent {
  public:
     /// DO NOT DELETE
     /// time in milliseconds after which the NextMove() answer is not sent to server, CAN BE CHANGED WHENEVER YOU WANT
-    int skipResponse = 90; // 100 - 10 to make sure overhead is included (100 is standard tick)
+    int skipResponse = 99;
 
 	Agent();
 	void Init(LobbyData lobbyData);
