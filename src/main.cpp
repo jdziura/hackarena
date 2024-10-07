6d781619a09991b244b558110b63baff9d54df36
Antoni Bombała <72938082+Bombinisss@users.noreply.github.com>
2024-10-07 20:12:15 +0200

    Added --help

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,32 @@ int main(int argc, char** argv) {
 	std::string port = "5000";
 	std::string nickname = "empty";
 	std::string code = "";
-    bool debugQuickJoin = false;
 
 	// Parse command line arguments into a map
 	std::unordered_map<std::string, std::string> args;
-	for (int i = 1; i < argc; ++i) {
-		std::string arg = argv[i];
-		if (arg.starts_with("--")) {
-			if (i + 1 < argc) {
-				args[arg.substr(2)] = argv[++i]; // Store key-value pairs
-			}
-		}
-	}
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg.starts_with("--")) {
+            // Handle flags like --help (no value expected)
+            if (arg == "--help") {
+                args["help"] = "";
+            } else if (i + 1 < argc && !argv[i + 1][0] == '-') {
+                // Store key-value pairs for arguments that expect a value
+                args[arg.substr(2)] = argv[++i];
+            }
+        }
+    }
 
 	// Update default values based on provided arguments
 	if (args.count("host")) host = args["host"];
 	if (args.count("port")) port = args["port"];
 	if (args.count("nickname")) nickname = args["nickname"];
 	if (args.count("code")) code = args["code"];
+
+    if (args.count("help")){
+        std::cout << "--nickname Nickname of the agent that will be displayed in the game.\nThis must be a unique identifier for the agent in the game environment.\nNicknames that are already in use or not unique will cause conflicts.\n--host The IP address or domain name of the server to connect to.\nThe agent will attempt to establish a connection to the specified host.\nIf not provided, it defaults to 'localhost'.\n--port The port on which the server is listening.\nThis specifies the port number that the server is using for communication.\nIf not provided, it defaults to port 5000.\n--code Optional access code required to join the server.\nIf the server enforces an access code for connections, it must be supplied here.\nIf no code is required, this can be left empty (default is an empty string)."
+                << std::endl << std::flush;
+    };
 
 	// Print the values
 	std::cout << "Host: " << host << std::endl << std::flush;
